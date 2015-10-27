dea2ff804bf64bd467763e0382a07ab6decaef20
cante <flordecante.ibanez@hp.com>
2015-10-27 14:58:31 +0800

    first!

---
+++ ShoppingCart/Security/CustomerIdentity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Security
+{
+    public class CustomerIdentity : ClaimsIdentity
+    {
+        internal CustomerIdentity(string authtype) : base(authtype) { }
+
+        internal CustomerIdentity(IEnumerable<Claim> claims) : base(claims) { }
+        public override bool IsAuthenticated
+        {
+            get
+            {
+                //TODO
+                return true;
+            }
+        }
+
+        public override string Name
+        {
+            get { return ""; }
+        }
+    }
+}
---
+++ ShoppingCart/Security/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ShoppingCart.Security
+{
+    internal static class CustomerService
+    {
+        internal static CustomerIdentity GetCustomerIdentity(string emailaddress)
+        {
+            var identity = new CustomerIdentity("Claims");
+            identity.AddClaim(new Claim("emailaddress", emailaddress));
+
+            return identity;
+        }
+
+        internal static CustomerIdentity GetCurrentUser()
+        {
+            var authorizationHeader = HttpContext.Current.Request.Headers["X-Jenni-Authorization"] ?? "";
+
+            // ::   if the bearer token is still the same as the original 
+            //      Authorization header, then this is not a Bearer token.
+            if (string.IsNullOrWhiteSpace(authorizationHeader))
+            {
+                return GetUserFromContext();
+            }
+            else
+            {
+                return TokenService.DecryptToken(authorizationHeader);
+            }
+        }
+
+        internal static CustomerIdentity GetUserFromContext()
+        {
+            var user = HttpContext.Current.User.Identity;
+            var emailAddress = user.Name;
+
+
+            return GetCustomerIdentity(emailAddress);
+        }
+    }
+}
